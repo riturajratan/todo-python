a6067b041059519b80600da2cc675832ddc91ddc
Rituraj <riturajratan@gmail.com>
2014-09-26 12:59:44 +0530

    getting problem in choice section

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+# here we are importing polls folder then read its file 
+# model.py in this i have getting Question class 
+from polls.models import Choice,Question
 # Register your models here.
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+
+admin.site.register(Choice)
+admin.site.register(Question, QuestionAdmin)
